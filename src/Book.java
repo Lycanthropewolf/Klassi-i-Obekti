f4dff1935870d5221963cc87aa72203bde5a083c
Антон <antonolegovich2017@icloud.com>
2022-08-27 19:59:26 +0600

    dz moe

--- src/Book.java
+++ src/Book.java
@@ -1,9 +1,15 @@
 public class Book {
-    String name;
-    int yearIssue;
+    private String name;
+    private int yearIssue;
 
     public Book(String name, int yearIssue){
         this.name= name;
         this.yearIssue=yearIssue;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public int getYearIssue(){
+        return this.yearIssue;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Book prestuplenie_I_nakazanie= new Book(" Преступление и наказание", 1866);
-        System.out.println(prestuplenie_I_nakazanie.name);
-        System.out.println(prestuplenie_I_nakazanie.yearIssue);
+        System.out.println(prestuplenie_I_nakazanie.getName());
+        System.out.println(prestuplenie_I_nakazanie.getYearIssue());
         Author dostoevskii = new Author("Достоевский", "Федор Михайлович");
-        System.out.println(dostoevskii.nameAuthor);
-        System.out.println(dostoevskii.lastNameAuthor);
+
+
     }
 
 
