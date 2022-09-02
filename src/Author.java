4f75c56592446d8ec7ee1ceb704d438186fde640
Антон <antonolegovich2017@icloud.com>
2022-09-02 18:04:26 +0600

    dz moe

--- src/Author.java
+++ src/Author.java
@@ -1,17 +1,28 @@
+import java.util.Objects;
+
 public class Author {
     private String nameAuthor;
     private String lastNameAuthor;
-    public Author (String nameAuthor, String lastNameAuthor) {
+
+    public Author(String nameAuthor, String lastNameAuthor) {
         this.nameAuthor = nameAuthor;
         this.lastNameAuthor = lastNameAuthor;
     }
-    public String getNameAuthor(){
-        return this.nameAuthor;
-      }
-    public String getLastNameAuthor() {
-        return this.lastNameAuthor;
+
+    public String getNameAuthor() {
+        return nameAuthor;
     }
-    public String toString(){
-        return " Автор - " + getNameAuthor()+" " + getLastNameAuthor();
+
+    public String getLastNameAuthor() {
+
+        return lastNameAuthor;
+    }
+
+    public String toString() {
+        return ", Автор - " + getNameAuthor() + " " + getLastNameAuthor();
+    }
+
+    public int hashCode(){
+        return Objects.hash(getNameAuthor().length());
     }
 }
